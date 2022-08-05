1c45327fb80af72a7031e97bde1d99a9937acf12
Michelsom P. da Silva <michelsonpagani@gmail.com>
2022-08-05 17:52:29 -0400

    fix:update in routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { HomeScreen } from "../screens/Home";
+import { Home } from "../screens/Home";
 const Stack = createNativeStackNavigator();
 
 export const AppRoutes = () => {
   return (
     <Stack.Navigator
-      initialRouteName="HomeScreen"
+      initialRouteName="Home"
       screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
 };
