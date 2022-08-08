5c7b5ba2f16ac45cb8cd5fcbc336fe5c982f8158
Michelsom <michelsonpagani@gmail.br>
2022-08-07 23:06:32 -0400

    feat: add test

--- src/__tests__/screens/home.spec.tsx
+++ src/__tests__/screens/home.spec.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 import { Home } from "../../screens/Home";
+import { App } from "../../App";
 
 test("check Home component", () => {
-  const { getByTestId, debug } = render(<Home />);
-  const home = getByTestId("home");
-  expect(home).toBeTruthy();
+  const { debug } = render(<App />);
+  debug();
 });
