008a3a1934497eb1b1e528c6548068d2507dde76
Michelsom <michelsonpagani@gmail.br>
2022-08-07 21:42:10 -0400

    fix:update in Home component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,29 +1,99 @@
 import React, { useEffect } from "react";
 import { ActivityIndicator, FlatList, StatusBar, View } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { HeaderHome } from "../../components/Header";
 import { Store } from "../../models/Store";
+import { getMovies } from "../../store/actions/movies.actions";
 import * as S from "./style";
+import * as Icons from "@expo/vector-icons";
 
 export const Home = () => {
-  const { moviesItems } = useSelector((store: Store) => store.movies);
+  const dispatch = useDispatch();
+  const { moviesItems, loadingMovies } = useSelector(
+    (store: Store) => store.movies
+  );
+
   const renderItem = ({ item }: any) => {
-    return <View>{/* <Text>{item.title}</Text> */}</View>;
+    return (
+      <S.BoxComponentFlatlist onPress={async () => {}}>
+        <S.ImageContainer
+          source={{
+            uri: `https://www.themoviedb.org/t/p/w500${item.image}`,
+          }}
+        >
+          <S.ContentFlatlist>
+            <S.PeopleWatched>{item.watchers} people watching</S.PeopleWatched>
+            <S.Row style={{ width: "100%" }}>
+              <S.MovieNameFlatlist>{item.movie.title}</S.MovieNameFlatlist>
+              <S.MovieYearFlatlist>{item.movie.year}</S.MovieYearFlatlist>
+            </S.Row>
+          </S.ContentFlatlist>
+        </S.ImageContainer>
+        <S.ContainerButton>
+          <S.Row>
+            <S.ButtonComponentFlatlist color={"#8e44ad"} onPress={() => {}}>
+              <Icons.Entypo name="check" size={24} color="white" />
+            </S.ButtonComponentFlatlist>
+            <S.ButtonComponentFlatlist color={"#62e9c9"} onPress={() => {}}>
+              <Icons.AntDesign name="book" size={24} color="white" />
+            </S.ButtonComponentFlatlist>
+            <S.ButtonComponentFlatlist color={"#74aed5"} onPress={() => {}}>
+              <Icons.MaterialCommunityIcons
+                name="format-list-text"
+                size={24}
+                color="white"
+              />
+            </S.ButtonComponentFlatlist>
+            <S.ButtonComponentFlatlist onPress={() => {}} color={"#fb7c2a"}>
+              <Icons.FontAwesome name="bolt" size={24} color="white" />
+            </S.ButtonComponentFlatlist>
+            <S.ButtonComponentFlatlist color={"#bcb6ac"} onPress={() => {}}>
+              <Icons.Ionicons name="md-play" size={24} color="white" />
+            </S.ButtonComponentFlatlist>
+          </S.Row>
+          <View>
+            <S.ButtonHeart onPress={() => {}}>
+              <Icons.Ionicons name="heart" size={22} color="#cc617a" />
+              <S.Percentage>100%</S.Percentage>
+            </S.ButtonHeart>
+          </View>
+        </S.ContainerButton>
+      </S.BoxComponentFlatlist>
+    );
   };
-  console.log(moviesItems);
+
+  function GetMoviesFn() {
+    dispatch(getMovies());
+  }
+  useEffect(() => {
+    GetMoviesFn();
+  }, []);
+
   return (
     <S.Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="#fff"
-        translucent={false}
-        hidden={false}
-      />
-      {/* <HeaderHome /> */}
-      {moviesItems?.length === 0 ? (
-        <ActivityIndicator size="large" color="#005" />
+      <StatusBar translucent={false} />
+      <HeaderHome />
+      {loadingMovies ? (
+        <S.BoxLoading style={{}}>
+          <ActivityIndicator size={90} color="#fff" />
+          <S.Title>Carregando filmes</S.Title>
+        </S.BoxLoading>
       ) : (
-        <FlatList data={moviesItems} renderItem={renderItem} />
+        <>
+          <FlatList
+            data={moviesItems}
+            ListHeaderComponent={
+              <View style={{ height: 100, width: "100%", padding: 10 }}>
+                <S.Title>Movies</S.Title>
+                <S.SubTitle>
+                  There are 2,308 people watching 1,102 movies right now!
+                </S.SubTitle>
+              </View>
+            }
+            ListFooterComponent={<View style={{ height: 50 }} />}
+            renderItem={renderItem}
+          />
+        </>
       )}
     </S.Container>
   );
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -3,7 +3,109 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   width: ${Dimensions.get("window").width}px;
-  height: ${Dimensions.get("window").height}px;
-  background-color: #fff;
+  height: ${Dimensions.get("screen").height}px;
+  background-color: #222;
 `;
 
+export const BoxComponentFlatlist = styled.TouchableOpacity`
+  width: 100%;
+  background-color: #222;
+`;
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+`;
+export const SubTitle = styled.Text`
+  color: #fff;
+  padding: 10px 0px;
+  font-size: 14px;
+`;
+export const ContentFlatlist = styled.View`
+  position: absolute;
+  width: 100%;
+  bottom: 20px;
+  left: 20px;
+`;
+export const MovieNameFlatlist = styled.Text.attrs({ numberOfLines: 1 })`
+  font-size: 27px;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.3);
+  height: 40px;
+  max-width: 306px;
+  font-weight: bold;
+`;
+export const Percentage = styled.Text`
+  font-size: 22px;
+  color: #fff;
+  max-width: 306px;
+  font-weight: bold;
+`;
+export const MovieYearFlatlist = styled.Text`
+  font-size: 23px;
+  background-color: rgba(0, 0, 0, 0.3);
+  height: 40px;
+  color: #fff;
+  padding-left: 5px;
+  padding-top: 5px;
+  color: rgba(255, 255, 255, 0.7);
+`;
+export const ImageContainer = styled.ImageBackground.attrs({
+  rezieMode: "contain",
+})`
+  background-color: #222;
+  width: 100%;
+  height: 250px;
+`;
+export const PeopleWatched = styled.Text`
+  background-color: #b70f15;
+  width: 163px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 15px;
+  color: #fff;
+  padding: 3px;
+`;
+
+interface IProps {
+  color: string;
+}
+export const ButtonComponentFlatlist = styled.TouchableOpacity<IProps>`
+  width: 48px;
+  height: 48px;
+  background-color: ${(color) => color.color};
+  align-items: center;
+  justify-content: center;
+`;
+export const ButtonHeart = styled.TouchableOpacity<IProps>`
+  height: 48px;
+  flex-direction: row;
+  width: 90px;
+  background-color: ${(color) => color.color};
+  align-items: center;
+  justify-content: center;
+`;
+export const ButtonComponViewntFlatlist = styled.TouchableOpacity`
+  width: 60px;
+  height: 60px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const ContainerButton = styled.View`
+  background-color: #222;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+export const BoxLoading = styled.View`
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
